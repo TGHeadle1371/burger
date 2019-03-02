0ce0ce77b3ab29cbbfaf3fb37ed84586816aeb41
TGHeadle1371 <tgheadle@gmail.com>
2019-03-02 14:31:24 -0700

    Database created, Burgers inserted into DB

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Santa Fe', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+
+
+SELECT * FROM burgers;
