2c2c418e49323b14a1132cfaeed9b9bbee26ca77
TGHeadle1371 <tgheadle@gmail.com>
2019-03-04 12:05:18 -0700

    added body parser

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({
+// Parse application
+app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
