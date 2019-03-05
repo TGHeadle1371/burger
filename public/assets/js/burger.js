82466fb22f6a24a6522afa56b4374433a5f735e7
TGHeadle1371 <tgheadle@gmail.com>
2019-03-05 14:29:33 -0700

    removed dotenv, added body-parser

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,40 +1,35 @@
 $(function () {
 
-    // Add a new burger.
+    // ADD NEW BURGER
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
 
         var newBurger = {
-            burger_name: $("#newburger").val().trim(),
+            burger_name: $("#newBurger").val().trim(),
             devoured: 0
         };
-
-        // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(function () {
-            console.log("Added new burger");
-            // Reload the page to get the updated burger list.
+            console.log("Added New Burger");
             location.reload();
         });
     });
 
-    $("#eatbutton").on("click", function (event) {
+    $(".eatburger").on("click", function (event) {
         event.preventDefault();
 
         var id = $(this).data("id");
-        var devouredState = {
+        var devouredBurger = {
             devoured: 1
         };
-
-        // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
-            data: devouredState
+            data: devouredBurger
         }).then(function () {
-            console.log("Burger devoured");
-            location.reload();
+            console.log("Burger Devoured");
+            location.reload;
         });
     });
 
@@ -43,11 +38,11 @@ $(function () {
 
         var id = $(this).data("id");
 
-        // Send the DELETE request.
         $.ajax({
             type: "DELETE",
             url: "/api/burgers/" + id
         }).then(location.reload());
     });
 
-})
+
+});
