3457545b68771f876d630b8cf35f00114f4dd162
TGHeadle1371 <tgheadle@gmail.com>
2019-03-05 15:41:59 -0700

    removed unneccessary dependency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controllers");
+var routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
